74d8e8bcaf891411c224fb226e86099987647216
Alaattin <123412972+alaattinkayalik@users.noreply.github.com>
2023-05-08 00:39:28 +0300

    string.py

---
+++ string.py
@@ -0,0 +1,5 @@
+firstName = input("Enter your name : ")
+lastName = input("Enter your surname : ")
+age = input("Enter your age : ")
+greeting = "Hello my name is "+firstName+ " " + lastName + " and I am " + str(age) + " Years Old"
+print(greeting)
