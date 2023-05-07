ca16048411f100edaacce006ebe905c48a0d1ede
Alaattin <123412972+alaattinkayalik@users.noreply.github.com>
2023-05-08 00:41:51 +0300

    Delete type-conversion-demo.py

--- type-conversion-demo.py
+++
@@ -1,6 +0,0 @@
-radiusN = float(input("please enter the radius : "))##
-pi = 3.14
-circleArea = pi*(radiusN ** 2)
-circlecircumference = 2 * pi * radiusN
-print(circleArea)
-print(circlecircumference)
