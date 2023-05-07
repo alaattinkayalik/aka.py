472fc204341af3e45208bdea9cc0fd6845b0fbfa
Alaattin <123412972+alaattinkayalik@users.noreply.github.com>
2023-05-08 00:42:00 +0300

    Delete string.py

--- string.py
+++
@@ -1,5 +0,0 @@
-firstName = input("Enter your name : ")
-lastName = input("Enter your surname : ")
-age = input("Enter your age : ")
-greeting = "Hello my name is "+firstName+ " " + lastName + " and I am " + str(age) + " Years Old"
-print(greeting)
